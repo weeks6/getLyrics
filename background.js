6d6508265fd57a8807ef48d7bf4d9bd7b531c76e
weeks6 <bridcarp1@gmail.com>
2018-01-12 02:58:14 +0600

    Last 12/1/18
    Fixed Youtube naming issue

--- background.js
+++ background.js
@@ -2,6 +2,7 @@ getLyrics = function (word) {
     var query = word.selectionText;
     query = query.replace(/\s+/g, '-');
     query = query.replace(/-–-/g, '-');
+    query = query.replace(/---/g, '-');
 
     chrome.tabs.create({
         url: "https://genius.com/" + query + '-lyrics'});
